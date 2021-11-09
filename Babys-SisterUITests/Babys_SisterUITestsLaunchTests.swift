9f118c520c1b9693ec4320cf1c5bc60d1f1b66b2
VithoriaS <vivilcsantos@gmail.com>
2021-11-09 20:04:47 -0300

    Initial Commit

---
+++ Babys-SisterUITests/Babys_SisterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Babys_SisterUITestsLaunchTests.swift
+//  Babys-SisterUITests
+//
+//  Created by Vitória Santos on 09/11/21.
+//
+
+import XCTest
+
+class Babys_SisterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
